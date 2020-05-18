80bccbdac5aaf3e6c22c3745fc66a7d172dfc1f7
Baudouin de Truchis <baudouin@detruchis.com>
2020-05-19 01:14:38 +0200

    get feet & head position

--- video_scraping.py
+++ video_scraping.py
@@ -1,1 +1,27 @@
-# test
+import os
+from datetime import datetime
+import requests
+import cv2
+from tqdm import tqdm
+
+
+def record_video(video_url, path_folder, location_name='none', frames=10):
+    for i in tqdm(range(frames), desc='Recording'):
+        # Get one frame
+        response = requests.get(video_url)
+        if response.status_code == 200:
+            raw_image = response.content
+
+        # Save image with timestamp
+        timestamp = str(round(datetime.utcnow().timestamp())) + '_' + str(round(datetime.utcnow().timestamp()*1000))[-3:]
+        filename = location_name + '_' + timestamp +'.jpg'
+        with open(path_folder + filename, 'wb') as file:
+            file.write(raw_image)
+
+
+# ========== RUN ==========
+
+path_folder = 'D:/code#/[large_data]/covid_project/' + 'recording/'
+video_url = 'http://85.71.106.87:60001/oneshotimage1?1589800293'
+
+record_video(video_url, path_folder, 'hranice', 1000)
