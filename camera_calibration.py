7927204ee72e3356a902862b52b79587b6fbf39d
Baudouin de Truchis <baudouin@detruchis.com>
2020-05-21 20:53:56 +0200

    calibration auto finished

--- camera_calibration.py
+++ camera_calibration.py
@@ -4,6 +4,7 @@ import numpy as np
 import random
 import cv2
 import cameratransform as ct
+from tqdm import tqdm
 
 
 # ========== FUNCTIONS ==========
@@ -64,7 +65,7 @@ def draw_predictions(frame, boxes, confidences, class_ids, labels, colors):
 	return frame
 
 def feet_head_position(boxes, class_ids, frame_height):
-    """Output feet & head position in the image"""
+    """Output feet & head position from bounding boxes"""
     position_feet = []
     position_head = []
     for i, box in enumerate(boxes):
@@ -73,7 +74,7 @@ def feet_head_position(boxes, class_ids, frame_height):
             position_feet.append([box[0]+box[2]//2, box[1]+box[3]])
             position_head.append([box[0]+box[2]//2, box[1]])
 
-    return np.asarray(position_feet), np.asarray(position_head)
+    return position_feet, position_head
 
 
 # ========== YOLO SETUP ==========
@@ -85,7 +86,7 @@ labels = open(path_folder + 'coco.names').read().strip().split("\n")
 colors = np.random.randint(0, 255, size=(len(labels), 3), dtype="uint8")
 
 # Load model
-print("[INFO] loading YOLO from disk...")
+print("[INFO] loading YOLO from disk")
 net = cv2.dnn.readNetFromDarknet(path_folder + 'yolov3.cfg', path_folder + 'yolov3.weights')
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
@@ -100,19 +101,39 @@ nms_threshold = 0.4		# Non-maximum suppression threshold : overlap maximum thres
 
 path_folder = 'D:/code#/[large_data]/covid_project/' + 'recording/'
 
-frame = plt.imread(path_folder + random.choice(os.listdir(path_folder)))
-# frame = plt.imread(path_folder + 'hranice_1589801140_244.jpg')
+feet = []
+heads = []
 
-(frame_height, frame_width) = frame.shape[:2]
+print('Collecting 100 positions: ', end='')
 
-# Transform frame in 416x416 blob + forward pass
-blob = cv2.dnn.blobFromImage(frame, 1/255, (416, 416), swapRB=True, crop=False)
-net.setInput(blob)
-outputs = net.forward(ln)
+for i in range(100):
+	frame = plt.imread(path_folder + random.choice(os.listdir(path_folder)))
+	# frame = plt.imread(path_folder + 'hranice_1589801140_244.jpg')
 
-# Post-processing
-boxes, confidences, class_ids = process_outputs(outputs, frame_width, frame_height, conf_threshold, nms_threshold)
-feet, heads = feet_head_position(boxes, class_ids, frame_height)
+	(frame_height, frame_width) = frame.shape[:2]
+
+	# Transform frame in 416x416 blob + forward pass
+	blob = cv2.dnn.blobFromImage(frame, 1/255, (416, 416), swapRB=True, crop=False)
+	net.setInput(blob)
+	outputs = net.forward(ln)
+
+	# Post-processing
+	boxes, confidences, class_ids = process_outputs(outputs, frame_width, frame_height, conf_threshold, nms_threshold)
+
+	feet_new, heads_new = feet_head_position(boxes, class_ids, frame_height)
+	feet.extend(feet_new)
+	heads.extend(heads_new)
+
+	print('■', end='')
+
+	# Stop calibration after 100 samples positions
+	if len(feet) > 100:
+		break
+
+print('\n[INFO] calibration finished')
+
+# Transform list --> matrix
+feet, heads = np.asarray(feet), np.asarray(heads)
 
 # Intrinsic camera parameters
 focal = 6.2                                                                         # in mm
@@ -122,20 +143,34 @@ frame_size = (frame.shape[1], frame.shape[0])                                   
 # Initialize the camera
 camera = ct.Camera(ct.RectilinearProjection(focallength_mm=focal,
                                             sensor=sensor_size,
-                                            image=frame_size))
+                                            image=frame))
 
 camera.addObjectHeightInformation(feet, heads, 1.7, 0.3)
 
+trace = camera.metropolis([
+		# ct.FitParameter("heading_deg", lower=-180, upper=180, value=0),
+        # ct.FitParameter("roll_deg", lower=-180, upper=180, value=0),
+        ct.FitParameter("elevation_m", lower=0, upper=100, value=5),
+        ct.FitParameter("tilt_deg", lower=0, upper=180, value=45)
+        ], iterations=1e4)
+
+plt.figure(1)
+camera.plotTrace()
+plt.tight_layout()
+
+plt.figure(2)
+camera.plotFitInformation(frame)
+plt.legend()
+
+plt.figure(3, figsize=(20,20))
+camera.getTopViewOfImage(frame, [-20, 20, 0, 75], do_plot=True)
+plt.xlabel("x position in m")
+plt.ylabel("y position in m")
+plt.show()
+
+
 frame = draw_predictions(frame, boxes, confidences, class_ids, labels, colors)
 
-
 # Display the frame
-# cv2.imshow('frame_undistorted', cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-# cv2.waitKey()
-
-
-# top_im = cam.getTopViewOfImage(frame, [-50, 50, 0, 200], scaling=0.5, do_plot=True)
-# plt.xlabel("x position in m")
-# plt.ylabel("y position in m")
-plt.imshow(frame)
-plt.show()
+cv2.imshow('frame', cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+cv2.waitKey()
