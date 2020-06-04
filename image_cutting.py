f5435edc9d9a960977dc0ae70e5610ce1e3dfdcf
EdinhoDSR <56434003+EdinhoDSR@users.noreply.github.com>
2020-06-04 11:58:59 +0200

    Images cutting with normal colors

--- image_cutting.py
+++ image_cutting.py
@@ -11,22 +11,40 @@ import numpy as np
 import random
 import cv2
 from camera_calibration import process_outputs
-from camera_calibration import draw_predictions
 
 # ========== FUNCTIONS ===========
 
-def cutting(frame, boxes, labels,class_ids):
+def cutting(name,frame, boxes, labels,class_ids):
+    cpt = 0
     #person
     """Take boxes in pixels with upper-left corner as reference --> draw bounding boxes on frame"""
     for i in range(len(boxes)):
         if labels[class_ids[i]] == "person":
+            cpt = cpt+1
             (x, y) = (boxes[i][0], boxes[i][1])
             (w, h) = (boxes[i][2], boxes[i][3])
             crop_img = frame[y:y+h, x:x+w]
-            cv2.imwrite('C:/Users/edinh/Desktop/ML/covid_project-master/test/test' + str(i) + '.png', crop_img)
             
-
-    
+            if crop_img.size != 0:
+                crop_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB)
+                cv2.imwrite('C:/Users/edinh/Desktop/ML/covid_project-master/data/' + name + '_cut_' + str(cpt) + '.png', crop_img)
+            
+            
+def cutting_head(frame, boxes, labels,class_ids):
+    cpt = 0
+    #person
+    """Take boxes in pixels with upper-left corner as reference --> draw bounding boxes on frame"""
+    for i in range(len(boxes)):
+        if labels[class_ids[i]] == "person":
+            cpt = cpt+1
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+            crop_img = frame[y:y+int(h/3), x:x+w]
+            
+            if crop_img.size != 0:
+                crop_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB)
+                cv2.imwrite('C:/Users/edinh/Desktop/ML/covid_project-master/data/' + name + '_head_cut_' + str(cpt) + '.png', crop_img)
+            
 # ========== YOLO SETUP ==========
 
 path_folder = 'C:/Users/edinh/Desktop/ML/covid_project-master/'
@@ -49,25 +67,25 @@ nms_threshold = 0.4		# Non-maximum suppression threshold : overlap maximum thres
 
 # ========== RUN ==========
 
-path_folder = 'C:/Users/edinh/Desktop/ML/covid_project-master/images/'
+path_folder = 'C:/Users/edinh/Desktop/ML/covid_project-master/images'
 
+for element in os.listdir(path_folder):
 
+    
+    
+    name = element.rsplit(".",1)[0]
+    frame = plt.imread(path_folder + '/' + element)
+    # frame = plt.imread(path_folder + 'hranice_1589801140_244.jpg')
+    
+    (frame_height, frame_width) = frame.shape[:2]
+    
+    # Transform frame in 416x416 blob + forward pass
+    blob = cv2.dnn.blobFromImage(frame, 1/255, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    outputs = net.forward(ln)
+    
+    # Post-processing
+    boxes, confidences, class_ids = process_outputs(outputs, frame_width, frame_height, conf_threshold, nms_threshold)
+    cutting(name, frame, boxes, labels, class_ids)
 
-print("Cutting an image")
-
-
-frame = plt.imread(path_folder + random.choice(os.listdir(path_folder)))
-# frame = plt.imread(path_folder + 'hranice_1589801140_244.jpg')
-
-(frame_height, frame_width) = frame.shape[:2]
-
-# Transform frame in 416x416 blob + forward pass
-blob = cv2.dnn.blobFromImage(frame, 1/255, (416, 416), swapRB=True, crop=False)
-net.setInput(blob)
-outputs = net.forward(ln)
-
-# Post-processing
-boxes, confidences, class_ids = process_outputs(outputs, frame_width, frame_height, conf_threshold, nms_threshold)
-cutting(frame, boxes, labels, class_ids)
-
-                                                       # in mm
+print('fini')                                                       # in mm
